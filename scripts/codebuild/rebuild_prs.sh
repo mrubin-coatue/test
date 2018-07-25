9bcb0a25356059b317a7fca630abcc3216116ce9
mrubin-coatue <40362258+mrubin-coatue@users.noreply.github.com>
2018-07-25 10:07:11 -0400

    Update rebuild_prs.sh

--- scripts/codebuild/rebuild_prs.sh
+++ scripts/codebuild/rebuild_prs.sh
@@ -1,2 +1,4 @@
+#!/usr/bin/env bash
+
 payload="{\"repo_url\": \"https://github.com/mrubin-coatue/test/\", \"codebuild_project_name\": \"dummy\", \"buildspec_filename\", \"another_dummy\"}"
 aws lambda invoke --function-name testFunction --invocation-type Event --payload $payload lambda_output.txt
