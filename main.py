8f83ae7bf22b625db9738eb30026be2af2c9d2b3
mrubin-coatue <40362258+mrubin-coatue@users.noreply.github.com>
2018-07-26 09:33:15 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,4 @@
-import other_file
-
-try:
-    import requests
-except Exception:
-    print("requests not found")
-    
-try:
-    from botocore.vendored import requests
-except Exception:
-    print("botocore not found")
-    
-try:
-    import boto3
-except Exception:
-    print("boto3 not found")
-    
-    
-
+import other_file    
 
 def my_func():
     other_file.good_func()
